38c75d32575bc0a66a730e55389613d93a150ac1
Bayardo <bayarloper@gmail.com>
2024-04-29 23:45:14 +0800

    backend error fixed

--- server/controllers/aboutController.js
+++ server/controllers/aboutController.js
@@ -6,19 +6,26 @@ const dbPath = path.resolve(__dirname, "../mydatabase.db");
 const db = new sqlite(dbPath);
 
 exports.getAboutPage = (req, res) => {
-    const data = db.prepare("SELECT title, description FROM about").get();
-    res.json(data);
+  const data = db.prepare("SELECT id, title, description FROM about").all();
+  res.json(data);
 };
 
 exports.updateAboutPage = (req, res) => {
-    const { id } = req.params;
-    const { description } = req.body;
+  const { id } = req.params;
+  const { description } = req.body;
 
-    try {
-        db.prepare("UPDATE about SET description = ? WHERE id = ?").run(description, id);
-        res.status(200).json({ message: "About page description updated successfully" });
-    } catch (error) {
-        console.error("Error updating about page description:", error);
-        res.status(500).json({ message: "Error updating about page description", error });
-    }
+  try {
+    db.prepare("UPDATE about SET description = ? WHERE id = ?").run(
+      description,
+      id
+    );
+    res
+      .status(200)
+      .json({ message: "About page description updated successfully" });
+  } catch (error) {
+    console.error("Error updating about page description:", error);
+    res
+      .status(500)
+      .json({ message: "Error updating about page description", error });
+  }
 };
